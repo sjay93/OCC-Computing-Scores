1eb762ba7c69f0936e59d5502148d7245788bedc
Jay Shah <s.jay93@gmail.com>
2020-02-18 18:34:47 -0600

    Setting system property for file name

--- src/main/java/com/occtakehome/computingscores/ComputingScoresApplication.java
+++ src/main/java/com/occtakehome/computingscores/ComputingScoresApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ComputingScoresApplication {
 
     public static void main(String[] args) {
+        System.setProperty("file.name",args[0]);
         SpringApplication.run(ComputingScoresApplication.class, args);
     }
 }
